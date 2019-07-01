acbe438d35d76e8b9d5df2d0be72e7e0ff06203f
Gjum <code.gjum@gmail.com>
2019-07-01 16:20:30 +0200

    UsefulCommandsModule, transform '/' (slash) commands into 'say'

--- src/main/kotlin/com/github/gjum/minecraft/botlin/Main.kt
+++ src/main/kotlin/com/github/gjum/minecraft/botlin/Main.kt
@@ -12,6 +12,8 @@ import com.github.gjum.minecraft.botlin.modules.ReloadableServiceRegistry
 import com.github.gjum.minecraft.botlin.modules.consumeService
 import com.github.gjum.minecraft.botlin.util.Cli
 import com.github.gjum.minecraft.botlin.util.Log
+import com.github.gjum.minecraft.botlin.util.toAnsi
+import com.github.steveice10.mc.protocol.packet.ingame.client.ClientChatPacket
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -26,9 +28,8 @@ object Main {
 		val modulesLoader = StaticModulesLoader(
 			setupDefaultModules() + MainArgsModule(args))
 		val serviceRegistry = ReloadableServiceRegistry(modulesLoader)
-		val reloadJob = GlobalScope.launch { serviceRegistry.reloadModules(null) }
+		GlobalScope.launch { serviceRegistry.reloadModules(null) }
 		trySetupCli(serviceRegistry)
-		runBlocking { reloadJob.join() }
 	}
 }
 
@@ -43,7 +44,8 @@ private fun setupDefaultModules() = listOf(
 	AvatarModule(),
 	CommandModule(),
 	EventLogger(),
-	IdlePhysics()
+	IdlePhysics(),
+	UsefulCommandsModule()
 )
 
 interface MainArgs : Service {
@@ -55,6 +57,42 @@ private class MainArgsModule(private val args: Array<String>) : Module() {
 		serviceRegistry.provideService(MainArgs::class.java, object : MainArgs {
 			override val args get() = this@MainArgsModule.args
 		})
+	}
+}
+
+class UsefulCommandsModule : Module() {
+	override suspend fun activate(serviceRegistry: ServiceRegistry) {
+		super.activate(serviceRegistry)
+		val commands = serviceRegistry.consumeService(CommandService::class.java)
+			?: return
+		val avatar = serviceRegistry.consumeService(Avatar::class.java)
+			?: return
+
+		commands.registerCommand("info", "info",
+			"Show bot info: connection, location, health, etc."
+		) { _, context ->
+			avatar.run {
+				context.respond("${profile.name} at $position $look on ${connection?.remoteAddress}")
+				context.respond("health=$health food=$food sat=$saturation exp=${experience?.total} (${experience?.level} lvl)")
+			}
+		}
+		commands.registerCommand("list", "list",
+			"Show connected players list."
+		) { _, context ->
+			val namesSpaceSep = avatar.playerList!!.values
+				.sortedBy { it.profile.name }
+				.joinToString(" ") {
+					it.displayName?.toAnsi() ?: it.profile.name
+				}
+			context.respond("Connected players: $namesSpaceSep")
+		}
+		commands.registerCommand("say", "say <message>",
+			"Send a chat message to the server."
+		) { cmdLine, context ->
+			val msg = cmdLine.substring("say ".length)
+			avatar.connection?.apply { send(ClientChatPacket(msg)) }
+				?: context.respond("Not connected to any server")
+		}
 	}
 }
 
@@ -71,11 +109,22 @@ private fun trySetupCli(services: ServiceRegistry) {
 		services.consumeService(CommandService::class.java)
 	} ?: return // no commands, so no cli
 	try {
-		Cli.run { cmdLine ->
-			if (cmdLine.trim().isEmpty()) return@run
+		commands.registerCommand("quit", "quit", "Close the program.") { _, _ ->
+			Cli.stop()
+		}
+		Cli.run { cmdLineRaw ->
+			if (cmdLineRaw.trim().isEmpty()) return@run
+			val isSlashCommand = cmdLineRaw.getOrNull(0)?.equals('/') == true
+			val cmdLine = if (isSlashCommand) "say $cmdLineRaw" else cmdLineRaw
 			val cmdName = cmdLine.split(' ')[0]
-			if (!commands.executeCommand(cmdLine, LoggingCommandContext(cmdName))) {
-				commandLogger.warning("Unknown command: $cmdLine")
+			val context = LoggingCommandContext(cmdName)
+			try {
+				if (!commands.executeCommand(cmdLine, context)) {
+					commandLogger.warning("Unknown command: $cmdLine")
+				}
+			} catch (e: Exception) {
+				context.respond("Error while running command: $e")
+				commandLogger.log(Level.WARNING, "Error while running command $cmdName", e)
 			}
 		}
 	} catch (e: Throwable) {
