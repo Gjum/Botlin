9ab5bd8677967fd61c1074ae4ffe0a0ecfc8d0c2
Gjum <code.gjum@gmail.com>
2020-03-23 04:31:04 +0100

    add entity metadata getters

--- src/main/kotlin/com/github/gjum/minecraft/botlin/api/World.kt
+++ src/main/kotlin/com/github/gjum/minecraft/botlin/api/World.kt
@@ -13,6 +13,7 @@ import com.github.steveice10.mc.protocol.data.game.entity.type.`object`.ObjectTy
 import com.github.steveice10.mc.protocol.data.game.world.block.BlockState
 import com.github.steveice10.mc.protocol.data.message.Message
 import java.util.UUID
+import kotlin.experimental.and
 
 /**
  * Properties that have not yet been received from the server are null.
@@ -29,7 +30,16 @@ interface Entity {
 	val vehicle: Entity?
 	val health: Float?
 	val metadata: Map<Int, EntityMetadata>
-	// TODO access mob metadata by name, via mc-data
+
+	// TODO access arbitrary entity metadata by name, via mc-data
+	val onFire: Boolean get() = metadata[0]?.let { (it.value as Byte).and(1) != 0.toByte() } ?: false
+	val crouching: Boolean get() = metadata[0]?.let { (it.value as Byte).and(2) != 0.toByte() } ?: false
+	val invisible: Boolean get() = metadata[0]?.let { (it.value as Byte).and(0x20) != 0.toByte() } ?: false
+	val glowing: Boolean get() = metadata[0]?.let { (it.value as Byte).and(0x40) != 0.toByte() } ?: false
+
+	val air: Int get() = metadata[1]?.let { it.value as Int } ?: 300
+	val potionEffectColor: Int get() = metadata[8]?.let { it.value as Int } ?: 300
+	val additionalHearts: Float get() = metadata[11]?.let { it.value as Float } ?: 0f
 }
 
 interface ExpEntity : Entity {
@@ -62,6 +72,9 @@ interface PlayerEntity : Entity {
 	val saturation: Float?
 	val experience: Experience?
 	val playerListItem: PlayerListItem?
+
+	val eyePos: Vec3d? get() = position?.plus(Vec3d(.0, eyeHeight, .0))
+	val eyeHeight: Double get() = if (crouching) 1.27 else 1.62
 }
 
 interface PlayerListItem {
