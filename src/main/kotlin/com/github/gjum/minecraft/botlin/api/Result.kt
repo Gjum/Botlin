b929432f865f425118b8ebbfa7f1490a923b751e
Gjum <code.gjum@gmail.com>
2020-03-24 19:50:50 +0100

    Result doesn't require throwable, only .getOrThrow() does

--- src/main/kotlin/com/github/gjum/minecraft/botlin/api/Result.kt
+++ src/main/kotlin/com/github/gjum/minecraft/botlin/api/Result.kt
@@ -1,11 +1,11 @@
 package com.github.gjum.minecraft.botlin.api
 
-sealed class Result<T, E : Throwable> {
+sealed class Result<T, E> {
 	open val value: T? get() = null
 	open val error: E? get() = null
 
-	class Success<T, E : Throwable>(override val value: T) : Result<T, E>()
-	class Failure<T, E : Throwable>(override val error: E) : Result<T, E>()
+	class Success<T, E>(override val value: T) : Result<T, E>()
+	class Failure<T, E>(override val error: E) : Result<T, E>()
 
 	operator fun component1(): T? = value
 	operator fun component2(): E? = error
@@ -16,7 +16,7 @@ fun <T, E : Throwable> Result<T, E>.getOrThrow(): T {
 	return value!!
 }
 
-fun <T, EO : Throwable, EI : EO> Result<Result<T, EI>, out EO>.flatten(): Result<T, out EO> {
+fun <T, EO, EI : EO> Result<Result<T, EI>, out EO>.flatten(): Result<T, out EO> {
 	return when (this) {
 		is Result.Success -> value
 		is Result.Failure -> Result.Failure(error)
