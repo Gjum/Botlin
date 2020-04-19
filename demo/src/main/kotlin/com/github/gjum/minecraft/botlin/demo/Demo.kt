e33f6b96607fa0c157664c5fd2f6f1d19f14a1c6
Gjum <code.gjum@gmail.com>
2020-04-20 01:50:54 +0200

    update demo to use runBotScript

--- demo/src/main/kotlin/com/github/gjum/minecraft/botlin/demo/Demo.kt
+++ demo/src/main/kotlin/com/github/gjum/minecraft/botlin/demo/Demo.kt
@@ -3,53 +3,58 @@ package com.github.gjum.minecraft.botlin.demo
 import com.github.gjum.minecraft.botlin.api.forward
 import com.github.gjum.minecraft.botlin.api.getOrThrow
 import com.github.gjum.minecraft.botlin.api.isSolid
-import com.github.gjum.minecraft.botlin.api.setupBot
+import com.github.gjum.minecraft.botlin.api.runBotScript
 import com.github.steveice10.mc.protocol.data.game.entity.player.Hand
 import com.github.steveice10.mc.protocol.packet.ingame.client.player.ClientPlayerSwingArmPacket
 import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
 
-fun main() = runBlocking {
-	setupBot("Botlin").run {
-		try {
-			connect("localhost:25565") // waits until spawned
+fun main() = runBotScript("Botlin") {
+	connect("localhost:25565") // waits until spawned
 
-			if (!alive) respawn() // check bot state
+	if (!alive) respawn() // check bot state
 
-			chat("hello") // rate limited chat
-			sendPacket(ClientPlayerSwingArmPacket(Hand.MAIN_HAND)) // send raw packets for not-yet implemented features
-			delay(1000)
-			println("jumping while moving")
+	chat("hello") // rate limited chat
+	sendPacket(ClientPlayerSwingArmPacket(Hand.MAIN_HAND)) // send raw packets for not-yet implemented features
 
-			val arrival = async { moveStraightBy(forward) }
-			jumpUntilLanded()
-			withTimeout(1000) { arrival.await() }.getOrThrow()
+	delay(1000)
+	println("searching inventory for any solid block (with a bounding box)")
 
-			delay(1000)
-			println("searching inventory for any solid block (with a bounding box)")
+	val slot = holdItem { slot ->
+		if (slot.empty) return@holdItem false
+		val block = slot.item.block
+			?: return@holdItem false // item is not a block
+		return@holdItem block.states[0].isSolid
+	}.getOrThrow()
 
-			val slot = holdItem { slot ->
-				if (slot.empty) return@holdItem false
-				val block = slot.item.block ?: return@holdItem false // item is not a block
-				return@holdItem block.states[0].isSolid
-			}.getOrThrow()
+	delay(1000)
+	println("pillaring up with ${slot.displayName}")
 
-			delay(1000)
-			println("pillaring up with ${slot.displayName}")
+	val floorPos = playerEntity!!.position!!
+	jumpByHeight(1.0)
+	placeBlock(floorPos)
 
-			val floorPos = playerEntity!!.position!!
-			jumpByHeight(1.0)
-			placeBlock(floorPos)
+	delay(1000)
+	println("jumping while moving")
+	coroutineScope {
+		// all actions inside this scope must complete before this scope completes
 
-			// TODO eating, crafting - using helper method
-			// TODO digging
-			// TODO duringReinforcementFortifying
-			// TODO swimming
-			// TODO vehicle movement
-		} finally {
-			disconnect()
+		val arrival = async {
+			moveStraightBy(forward)
 		}
+		// while moving, jump
+		jumpUntilLanded()
+
+		withTimeout(1000) {
+			arrival.await()
+		}.getOrThrow()
 	}
+
+	// TODO eating, crafting - using helper method
+	// TODO digging
+	// TODO duringReinforcementFortifying
+	// TODO swimming
+	// TODO vehicle movement
 }
